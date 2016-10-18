077ff47f0a2dd67899befebcc49cf9b1ee7f6759
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-10-18 10:39:56 +0900

    add first alert

--- src/js/page/Index.js
+++ src/js/page/Index.js
@@ -6,71 +6,76 @@ export default class Index {
   }
 
   initialize() {
-    var $body = $('body');
+    this.$body = $('body');
 
+    var firstAudio = new Audio();
     var onAudio = new Audio();
     var offAudio = new Audio();
 
     var isWired = true;
 
+    firstAudio.src = './audio/first.mp3';
     onAudio.src = './audio/on.mp3';
     offAudio.src = './audio/off.mp3';
 
-    setInterval(function() {
-      var time = new Date();
-      var timeStr = time.toLocaleString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: false,
-      });
+    firstAudio.play();
+    this.alert({
+      text: 'ネットが切れたらお知らせするよー',
+    });
 
+    setInterval(() => {
       $.ajax(`./js/online.js?${Date.now()}`, {
-        success: function(res) {
+        success: (res) => {
           console.log('success');
-
-          let $content;
-          let $time;
 
           if(isWired === null) {
             isWired = true;
           } else if(isWired === false) {
-            $time = $('<time></time>')
-              .text(timeStr)
-            ;
-            $content = $('<p></p>')
-              .text(' ネットがつながったー')
-              .prepend($time)
-            ;
-            $body.append($content);
+            this.alert({
+              text: 'ネットがつながったー',
+            });
 
             onAudio.play();
           }
           isWired = true;
         },
-        error: function(err) {
+        error: (err) => {
           console.log('failed');
-
-          let $content;
-          let $time;
 
           if(isWired === null) {
             isWired = false;
           } else if(isWired === true) {
-            $time = $('<time></time>')
-              .text(timeStr)
-            ;
-            $content = $('<p></p>')
-              .text(' ネットが切れたー')
-              .prepend($time)
-            ;
-            $body.append($content);
-
             offAudio.play();
+
+            this.alert({
+              text: 'ネットが切れたー',
+            });
           }
+
           isWired = false;
         },
       });
     }, 1000);
+  }
+
+  alert(opts = {}) {
+    var time = new Date();
+    var timeStr = time.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+    });
+
+    var text = opts.text;
+
+    var $time = $('<time></time>')
+      .text(timeStr)
+    ;
+    var $content = $('<p></p>')
+      .text(` ${text}`)
+      .prepend($time)
+    ;
+    this.$body.append($content);
   }
 }
